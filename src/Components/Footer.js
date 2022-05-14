97d5a17f17e0240885ccc1158d536d34dc5906e7
MR-DHRUV <dhruvgupta742@gmail.com>
2022-05-14 14:00:38 +0530

    Footer fixed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,9 +10,9 @@ const Footer = () => {
         <footer>
             <h4 className="head h4 my-3">IPURankList.com</h4>
             <div className="social my-3">
-                <Link to='/'><img className='socialicons mx-3' src={require('./images/github.png')} alt="NA" srcset="" height={'30px'} width={'30px'} /></Link>
-                <Link to='/'><img className='socialicons mx-3' src={require("./images/instagram.png")} alt="NA" srcset="" height={'30px'} width={'30px'} /></Link>
-                <Link to='/'><img className='socialicons mx-3' src={require("./images/linkedin.png")} alt="NA" srcset="" height={'30px'} width={'30px'} /></Link>
+                <Link to='/'><img className='socialicons mx-3 my-f-img' src={require('./images/github.png')} alt="NA" srcset="" height={'30px'} width={'30px'} /></Link>
+                <Link to='/'><img className='socialicons mx-3 my-f-img' src={require("./images/instagram.png")} alt="NA" srcset="" height={'30px'} width={'30px'} /></Link>
+                <Link to='/'><img className='socialicons mx-3 my-f-img' src={require("./images/linkedin.png")} alt="NA" srcset="" height={'30px'} width={'30px'} /></Link>
             </div>
             <div className="footerc">
                 <h5 className="h5 copyright myfootertext my-3">Copyright &copy; {year} All rights reserved | <Link className='Sitelink' to='/'>IPUranklist.com</Link></h5>
